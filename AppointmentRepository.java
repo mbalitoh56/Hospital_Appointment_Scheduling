acf518099f1ccf44ad57ef9a930b437853cb5b18
Nomthandazo Mbali Mavundla <mbalitoh56@gmail.com>
2025-04-27 18:48:36 +0200

    Update AppointmentRepository.java

--- AppointmentRepository.java
+++ AppointmentRepository.java
@@ -1,1 +1,10 @@
+package repositories;
 
+import hospitalappointmentsystem.Appointment;
+
+/**
+ * Specific repository interface for Appointment entities.
+ */
+public interface AppointmentRepository extends Repository<Appointment, String> {
+    // No extra methods needed for now
+}
