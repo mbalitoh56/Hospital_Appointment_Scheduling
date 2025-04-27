d6d4942811afb30a028011b618179bf78ebdaf9e
Nomthandazo Mbali Mavundla <mbalitoh56@gmail.com>
2025-04-27 18:46:15 +0200

    Update Repository.java

--- Repository.java
+++ Repository.java
@@ -1,1 +1,18 @@
+package repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * A generic Repository interface for basic CRUD operations.
+ * 
+ * @param <T> Entity type
+ * @param <ID> ID type
+ */
+public interface Repository<T, ID> {
+    void save(T entity);              // Create/Update
+    Optional<T> findById(ID id);       // Read (Single)
+    List<T> findAll();                 // Read (All)
+    void delete(ID id);                // Delete
+}
 
