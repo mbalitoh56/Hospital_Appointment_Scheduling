31b3aa7e927dad92b2b66768773a7f5da0c86fc9
Nomthandazo Mbali Mavundla <mbalitoh56@gmail.com>
2025-04-27 18:58:28 +0200

    Update RepositoryFactory.java

--- RepositoryFactory.java
+++ RepositoryFactory.java
@@ -1,1 +1,18 @@
+package factories;
 
+import repositories.AppointmentRepository;
+import repositories.inmemory.InMemoryAppointmentRepository;
+
+public class RepositoryFactory {
+
+    public static AppointmentRepository getAppointmentRepository(String storageType) {
+        switch (storageType.toUpperCase()) {
+            case "MEMORY":
+                return new InMemoryAppointmentRepository();
+            // case "DATABASE":
+            //     return new DatabaseAppointmentRepository(); // Future implementation
+            default:
+                throw new IllegalArgumentException("Invalid storage type: " + storageType);
+        }
+    }
+}
