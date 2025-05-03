42e0ac3485507422e2dc6b592d61ddf93b5bb5dc
Nomthandazo Mbali Mavundla <mbalitoh56@gmail.com>
2025-05-03 14:46:53 +0200

    Create PatientController.java

---
+++ Assignment 12/api/PatientController.java
@@ -0,0 +1,40 @@
+package api;
+
+import models.Patient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import services.PatientService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patients")
+public class PatientController {
+    private final PatientService patientService;
+
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Patient>> getAllPatients() {
+        return ResponseEntity.ok(patientService.findAllPatients());
+    }
+
+    @PostMapping
+    public ResponseEntity<Patient> createPatient(@RequestBody Patient patient) {
+        return ResponseEntity.ok(patientService.createPatient(patient));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Patient> updatePatient(@PathVariable String id, @RequestBody Patient patient) {
+        patient.setId(id);
+        return ResponseEntity.ok(patientService.updatePatient(patient));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable String id) {
+        patientService.deletePatient(id);
+        return ResponseEntity.ok().build();
+    }
+}
